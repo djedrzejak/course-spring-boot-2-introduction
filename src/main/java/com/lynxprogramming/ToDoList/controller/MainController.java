518f63032cac3d965cb68fe933e8b48589a7ab9e
djedrzejak <djedrzejak@gmail.com>
2021-03-21 10:16:53 +0100

    complete application

---
+++ src/main/java/com/lynxprogramming/ToDoList/controller/MainController.java
@@ -0,0 +1,53 @@
+package com.lynxprogramming.ToDoList.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lynxprogramming.ToDoList.model.Task;
+import com.lynxprogramming.ToDoList.repository.TaskRepository;
+
+@Controller
+public class MainController {
+
+	@Autowired
+	private TaskRepository taskRepository;
+	
+	@RequestMapping("/")
+	public String getTasks(Model model) {
+		List<Task> tasks = taskRepository.findAll();
+		model.addAttribute("tasks", tasks);
+		return "index";
+	}
+	
+	@RequestMapping("/tasks/{id}")
+	public String closeTask(@PathVariable int id) {
+		taskRepository.deleteById(id);
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/newTask")
+	public String getTaskForm() {
+		return "task-form";
+	}
+	
+	@PostMapping("/processForm")
+	public String addTask(@ModelAttribute Task task) {
+		taskRepository.save(task);
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/sortByPriority")
+	public String getAllByPriority(Model model) {
+		List<Task> tasks = taskRepository.findAllByOrderByPriority();
+		model.addAttribute("tasks", tasks);
+		return "index";
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into task (id, priority, description, date) values
+(101, 2, 'Pay bills', '2021-01-20'),
+(102, 3, 'Call John', '2021-01-21'),
+(103, 1, 'Develop new feature in my app', '2021-01-17'),
+(104, 2, 'Prepare for presentation', '2021-01-18');
